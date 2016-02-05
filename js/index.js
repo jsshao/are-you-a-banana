2ba80438a25906978b8a9e144f254533b434858f
Jason Shao <jason.shao@snapchat.com>
2016-02-05 01:30:04 -0800

    add uploader

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#uploader").change(function() {
+        $("#path").val($("#uploader").val().split('\\').pop());
+    });
+
+    $("#browse").click(function(e) {
+        $('input[type=file]').click();
+    })
+});
